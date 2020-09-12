d2405e234170e15086fb4f18d52a938d852bd409
Clément Décou <clementdecou@mbp-de-clement.home>
2020-09-12 16:35:17 +0200

    start dev

--- simply-framework.php
+++ simply-framework.php
@@ -1,5 +1,6 @@
 <?php
 
+use SimplyFramework\Command\ClearCacheCommand;
 use Symfony\Component\Config\ConfigCache;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -9,30 +10,46 @@ use Symfony\Component\Finder\Finder;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+class Simply {
+    private static $container;
+    private static function initContainer() {
+        $file = __DIR__ .'/cache/container.php';
+        $containerConfigCache = new ConfigCache($file, WP_DEBUG);
+        $configDir = __DIR__ . '/config';
 
-$file = __DIR__ .'/cache/container.php';
-$containerConfigCache = new ConfigCache($file, WP_DEBUG);
-$configDir = __DIR__ . '/config';
+        if (!$containerConfigCache->isFresh()) {
+            $containerBuilder = new ContainerBuilder();
 
-if (!$containerConfigCache->isFresh()) {
-    $containerBuilder = new ContainerBuilder();
+            $finder = new Finder();
+            $finder->files()->in($configDir);
+            $loader = new YamlFileLoader($containerBuilder, new FileLocator($configDir));
+            if ($finder->hasResults()) {
+                foreach ($finder as $aFile) {
+                    $loader->load($aFile->getRelativePathname());
+                }
+            }
+            $containerBuilder->compile();
 
-    $finder = new Finder();
-    $finder->files()->in($configDir);
-    $loader = new YamlFileLoader($containerBuilder, new FileLocator($configDir));
-    if ($finder->hasResults()) {
-        foreach ($finder as $aFile) {
-            $loader->load($aFile->getRelativePathname());
+            $dumper = new PhpDumper($containerBuilder);
+            $containerConfigCache->write(
+                $dumper->dump(['class' => 'CachedContainer']),
+                $containerBuilder->getResources()
+            );
         }
-    }
-    $containerBuilder->compile();
 
-    $dumper = new PhpDumper($containerBuilder);
-    $containerConfigCache->write(
-        $dumper->dump(['class' => 'CachedContainer']),
-        $containerBuilder->getResources()
-    );
+        require_once $file;
+        self::$container = new CachedContainer();
+    }
+
+    static function getContainer() {
+        if (is_null(self::$container)) {
+            self::initContainer();
+        }
+        return self::$container;
+    }
 }
 
-require_once $file;
-$container = new CachedContainer();
+add_action('cli_init', function() {
+    new ClearCacheCommand();
+});
+
--- src/Command/ClearCacheCommand.php
+++ src/Command/ClearCacheCommand.php
@@ -2,8 +2,10 @@
 
 namespace SimplyFramework\Command;
 
+use Symfony\Component\Filesystem\Filesystem;
+
 class ClearCacheCommand extends AbstractWordPressCommand {
     function execute($args, $assoc_args) {
-        
+        $fs = new Filesystem();
     }
 }
