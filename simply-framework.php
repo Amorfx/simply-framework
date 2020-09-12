17747cfd37635fc9c78bc2af0ac276bbfe3dbc8d
Clément Décou <clementdecou@mbp-de-clement.home>
2020-09-12 16:26:55 +0200

    start dev

--- simply-framework.php
+++ simply-framework.php
@@ -1,25 +1,40 @@
 <?php
 
+use Symfony\Component\Config\ConfigCache;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Finder\Finder;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 
-// based on something in your project
-
 $file = __DIR__ .'/cache/container.php';
-$containerConfigCache = new ConfigCache($file, $isDebug);
+$containerConfigCache = new ConfigCache($file, WP_DEBUG);
+$configDir = __DIR__ . '/config';
 
 if (!$containerConfigCache->isFresh()) {
     $containerBuilder = new ContainerBuilder();
-    // ...
+
+    $finder = new Finder();
+    $finder->files()->in($configDir);
+    $loader = new YamlFileLoader($containerBuilder, new FileLocator($configDir));
+    if ($finder->hasResults()) {
+        foreach ($finder as $aFile) {
+            $loader->load($aFile->getRelativePathname());
+        }
+    }
     $containerBuilder->compile();
 
     $dumper = new PhpDumper($containerBuilder);
     $containerConfigCache->write(
-        $dumper->dump(['class' => 'MyCachedContainer']),
+        $dumper->dump(['class' => 'CachedContainer']),
         $containerBuilder->getResources()
     );
 }
 
 require_once $file;
-$container = new MyCachedContainer();
+$container = new CachedContainer();
+
+var_dump($container->getParameter('framework.version')); die;
---
+++ src/Command/AbstractWordPressCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace SimplyFramework\Command;
+
+abstract class AbstractWordPressCommand {
+    static $commandName;
+    static $requiredArgs = array();
+
+    public function __construct() {
+        \WP_CLI::add_command($this::$commandName, array($this, '_execute'));
+    }
+
+    /**
+     * Show log with color
+     * use correct color of WP_CLI
+     * See https://make.wordpress.org/cli/handbook/internal-api/wp-cli-colorize/
+     * @param $message
+     * @param string $color
+     */
+    protected function showColorMessage($message, $color = '%n') {
+        \WP_CLI::log(\WP_CLI::colorize($color.$message));
+    }
+
+    /**
+     * Add question for command
+     * @param $question
+     * @param array $assoc_args
+     */
+    protected function confirm($question, $assoc_args = array()) {
+        \WP_CLI::confirm($question, $assoc_args);
+    }
+
+    /**
+     * Verify required args of the command
+     * @param $assoc_args
+     */
+    protected function verifyRequiredArgs($assoc_args) {
+        $missedArgs = array();
+        if (is_array($this::$requiredArgs) && ! empty($this::$requiredArgs)) {
+            foreach ($this::$requiredArgs as $anArg) {
+                if (!array_key_exists($anArg, $assoc_args)) {
+                    $missedArgs[] = $anArg;
+                }
+            }
+        }
+        if (!empty($missedArgs)) {
+            $this->showColorMessage('The arg(s) ' . implode(',', $missedArgs) . ' missed.', '%r');
+            exit();
+        }
+    }
+
+    /**
+     * Main execute function for all commands
+     * @param $args
+     * @param $assoc_args
+     */
+    public function _execute($args, $assoc_args) {
+        // Exit if not have required args
+        $this->verifyRequiredArgs($assoc_args);
+
+        // If have all required args
+        $this->execute($args, $assoc_args);
+    }
+
+    abstract function execute($args, $assoc_args);
+}
