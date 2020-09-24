6eee815fc6453c648d435e703669b7500916006a
Clément Décou <clementdecou@MacBook-Pro-de-Clement.local>
2020-09-24 18:48:11 +0200

    feat: add Shortcode manager and abstract shortcode class

---
+++ src/Manager/ShortcodeManager.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SimplyFramework\Manager;
+
+use SimplyFramework\Contract\ManagerInterface;
+use SimplyFramework\Shortcode\AbstractShortcode;
+
+class ShortcodeManager implements ManagerInterface {
+    /**
+     * @var AbstractShortcode[]
+     */
+    private $shortcodes;
+
+    public function __construct($shortcodes) {
+
+        $this->shortcodes = $shortcodes;
+    }
+
+    public function initialize() {
+        add_action('init', function() {
+            foreach ($this->shortcodes as $aShortcode) {
+                if (!$aShortcode instanceof AbstractShortcode) {
+                    throw new \RuntimeException('Services with tags wp_shortcode has to be an extension class of AbstractShortcode');
+                }
+
+                $aShortcode->register();
+            }
+        });
+    }
+
+    /**
+     * Get a shortcode by its tag or classname
+     * @param $key
+     *
+     * @return false|AbstractShortcode
+     */
+    public function getShortcode($key) {
+        foreach ($this->shortcodes as $aShortcode) {
+            if ($aShortcode->getTag() === $key || get_class($aShortcode) === $key) {
+                return $aShortcode;
+            }
+        }
+         return false;
+    }
+}
