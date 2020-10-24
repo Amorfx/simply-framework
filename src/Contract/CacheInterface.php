d758dbc67655bcaf131e19764c0e222f33d13e3b
Clément Décou <clementdecou@macbook-pro-de-clement.home>
2020-10-24 23:10:39 +0200

    feat: add Service Cache and sample theme and default theme command

---
+++ src/Contract/CacheInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SimplyFramework\Contract;
+
+interface CacheInterface {
+    /**
+     * Get cache value from key
+     * @param $key
+     *
+     * @return mixed
+     */
+    public function get($key);
+
+    /**
+     * Set value to cache
+     *
+     * @param $key
+     * @param $value
+     * @param $expire
+     *
+     * @return mixed
+     */
+    public function set($key, $value, $expire = null);
+}
