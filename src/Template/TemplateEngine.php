7630b481f8fb19308503751eac948db5ddd81107
Clément Décou <clementdecou@macbook-pro-de-clement.home>
2020-11-25 09:40:18 +0100

    refactor: remove unuse and change composer.json

--- src/Template/TemplateEngine.php
+++ src/Template/TemplateEngine.php
@@ -14,17 +14,8 @@ class TemplateEngine {
     private $engine;
 
     public function __construct() {
-        // the Twig file that holds all the default markup for rendering forms
-        // this file comes with TwigBridge
-        $defaultFormTheme = 'admin/metabox/form_div_layout.html.twig';
-
-        // the path to TwigBridge library so Twig can locate the
-        // form_div_layout.html.twig file
-        $appVariableReflection = new \ReflectionClass('\Symfony\Bridge\Twig\AppVariable');
-        $vendorTwigBridgeDirectory = dirname($appVariableReflection->getFileName());
         // the path to your other templates
-        $viewsDirectory = [realpath(__DIR__.'/../../views')];
-        $viewsDirectory = apply_filters('simply_views_directory', $viewsDirectory);
+        $viewsDirectory = apply_filters('simply_views_directory', []);
 
         // environnement configuration
         $envConfig = [
@@ -35,12 +26,6 @@ class TemplateEngine {
         }
 
         $twig = new Environment(new FilesystemLoader($viewsDirectory), $envConfig);
-        $formEngine = new TwigRendererEngine([$defaultFormTheme], $twig);
-        $twig->addRuntimeLoader(new FactoryRuntimeLoader([
-            FormRenderer::class => function () use ($formEngine) {
-                return new FormRenderer($formEngine);
-            }
-        ]));
 
         // adds the FormExtension to Twig
         $twig->addExtension(new FormExtension());
