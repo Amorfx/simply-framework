cd170e5ca7b13d089476d38cabe21f1a79845351
Clément Décou <clement.decou@ornikar.com>
2024-02-23 19:31:42 +0100

    wip: better code

--- src/Command/DebugHookCommand.php
+++ src/Command/DebugHookCommand.php
@@ -14,6 +14,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class DebugHookCommand extends Command
 {
     protected static $defaultName = 'simply:debug:hook';
+    private InputInterface $input;
+    private OutputInterface $output;
 
     protected function configure()
     {
@@ -23,63 +25,98 @@ final class DebugHookCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // Load full wordpress : plugin and theme
-            global $wp_filter;
-            $hooks = $wp_filter;
-            $filter = $input->getOption('filter');
-            if (array_key_exists($filter, $hooks)) {
-                $hooks = [$filter => $hooks[$filter]];
-            } elseif ($filter) {
-                $hooks = [];
-            }
-            $hook_names = array_keys($hooks);
-            sort($hook_names);
-            /** @var HookDebug[] $hookDebug */
-            $hookDebug = [];
-            foreach ($hooks as $name => $hookData) {
-                if (empty($hookData->callbacks)) {
-                    continue;
-                }
+        $this->input = $input;
+        $this->output = $output;
 
-                foreach ($hookData->callbacks as $priority => $callbacks) {
-                    foreach ($callbacks as $dataCallback) {
-                        $source = '';
-                        $functionName = '';
-                        $sourceLine = 0;
-                        if (is_array($dataCallback['function'])) {
-                            $reflector = new ReflectionMethod($dataCallback['function'][0], $dataCallback['function'][1]);
-                            $source = $reflector->getFileName();
-                            $functionName = $dataCallback['function'][1];
-                            $sourceLine = $reflector->getStartLine();
-                        } elseif (is_string($dataCallback['function'])) {
-                            $reflector = new ReflectionFunction($dataCallback['function']);
-                            $source = $reflector->getFileName();
-                            $functionName = $dataCallback['function'];
-                            $sourceLine = $reflector->getStartLine();
-                        }
-                        $hookDebug[] = new HookDebug(
-                            $name,
-                            $source,
-                            $sourceLine,
-                            $functionName,
-                            $priority,
-                        );
+        $this->loadAllWordpressFiles();
+
+        $filter = $input->getOption('filter');
+        $hooks = $this->getAllHooksRegistered($filter);
+        $hook_names = array_keys($hooks);
+        sort($hook_names);
+
+        $hookDebug = $this->constructHookDebugData($hooks);
+
+        $table = new Table($output);
+        $table->setStyle('box');
+        $table->setHeaders(['Hook Name', 'Priority', 'method/function name', 'Source']);
+        foreach ($hookDebug as $hook) {
+            $table->addRow([
+                $hook->name,
+                $hook->priority,
+                $hook->functionName,
+                "<href=file://$hook->source>$hook->source line $hook->sourceLine</>",
+            ]);
+        }
+        $table->render();
+
+        return Command::SUCCESS;
+    }
+
+    private function getAllHooksRegistered(?string $filter = null): array
+    {
+        global $wp_filter;
+        if (array_key_exists($filter, $wp_filter)) {
+            return [$filter => $wp_filter[$filter]];
+        }
+
+        if ($filter) {
+            $this->output->writeln("<error>Hook $filter not found.</error>");
+            return [];
+        }
+
+        return $wp_filter;
+    }
+
+    private function constructHookDebugData(array $hooks): array
+    {
+        /** @var HookDebug[] $hookDebug */
+        $hookDebug = [];
+        foreach ($hooks as $name => $hookData) {
+            if (empty($hookData->callbacks)) {
+                continue;
+            }
+
+            foreach ($hookData->callbacks as $priority => $callbacks) {
+                foreach ($callbacks as $dataCallback) {
+                    $source = '';
+                    $functionName = '';
+                    $sourceLine = 0;
+                    if (is_array($dataCallback['function'])) {
+                        $reflector = new ReflectionMethod($dataCallback['function'][0], $dataCallback['function'][1]);
+                        $source = $reflector->getFileName();
+                        $functionName = $dataCallback['function'][1];
+                        $sourceLine = $reflector->getStartLine();
+                    } elseif (is_string($dataCallback['function'])) {
+                        $reflector = new ReflectionFunction($dataCallback['function']);
+                        $source = $reflector->getFileName();
+                        $functionName = $dataCallback['function'];
+                        $sourceLine = $reflector->getStartLine();
                     }
+                    $hookDebug[] = new HookDebug(
+                        $name,
+                        $source,
+                        $sourceLine,
+                        $functionName,
+                        $priority,
+                    );
                 }
             }
-            $table = new Table($output);
-            $table->setStyle('box');
-            $table->setHeaders(['Hook Name', 'Priority', 'method/function name', 'Source']);
-            foreach ($hookDebug as $hook) {
-                $table->addRow([
-                    $hook->name,
-                    $hook->priority,
-                    $hook->functionName,
-                    "<href=file://$hook->source>$hook->source line $hook->sourceLine</>",
-                ]);
-            }
-            $table->render();
+        }
 
-            return Command::SUCCESS;
+        return $hookDebug;
+    }
+
+    private function loadAllWordpressFiles(): void
+    {
+        // Display all hooks => load admin files too
+        require_once ABSPATH . 'wp-admin/includes/admin.php';
+        require_once ABSPATH . 'wp-admin/includes/ajax-actions.php';
+        require_once ABSPATH . 'wp-admin/includes/dashboard.php';
+        require_once ABSPATH . 'wp-admin/includes/nav-menu.php';
+        require_once ABSPATH . 'wp-admin/includes/plugin-install.php';
+        require ABSPATH . 'wp-admin/includes/theme-install.php';
+        require ABSPATH . 'wp-admin/includes/update-core.php';
+        require ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
     }
 }
